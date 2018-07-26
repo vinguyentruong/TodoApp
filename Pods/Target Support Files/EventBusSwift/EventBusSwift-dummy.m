75f0755386d5f14b13bf2714293b8033a650837f
David Nguyen Truong <>
2018-07-26 11:12:40 +0700

    remove pod

--- Pods/Target Support Files/EventBusSwift/EventBusSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_EventBusSwift : NSObject
-@end
-@implementation PodsDummy_EventBusSwift
-@end
--- Pods/Target Support Files/Pods-TodoProjectUITests/Pods-TodoProjectUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TodoProjectUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TodoProjectUITests
-@end
